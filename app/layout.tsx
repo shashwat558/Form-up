85864963d0a6c3cbac110f60b722a564812c3942
shashwat558 <jainshashwat528@gmail.com>
2024-12-28 23:21:26 +0530

    creating file structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "./providers/ThemeProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,11 +26,21 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
+      
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+         defaultTheme="dark" 
+        
+        >
+           {children}
+        </ThemeProvider>
+        
       </body>
+      
     </html>
     </ClerkProvider>
   );
